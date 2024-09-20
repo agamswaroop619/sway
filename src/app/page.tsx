659493d19e6d561931a501656e2c58ee53ef30e0
vinod-r07 <vkumar.vinod2021@gmail.com>
2024-09-20 23:34:25 +0530

    commit 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,44 +3,7 @@
 
 
 export default function Home() {
-  const data = [
-    {
-      id: 1,
-      title: "Image 1",
-      image1:
-        "https://media.istockphoto.com/id/618976674/photo/fashion-woman-shopping.jpg?s=1024x1024&w=is&k=20&c=OVwkrGYD2ylReVjw_bs2rBQWt4QnP7cNbffJWoUCWWI=",
-      image2:
-        "https://media.istockphoto.com/id/1462655622/photo/beautiful-woman-in-a-leather-suit-looking-masculine.jpg?s=1024x1024&w=is&k=20&c=nFOAF-146OMBFNaETqvTywaxWj15KA8WpbXtyhCCDxw=",
-    },
-    {
-      id: 2,
-      title: "Image 2",
-      image1:
-        "https://images.pexels.com/photos/432059/pexels-photo-432059.jpeg?auto=compress&cs=tinysrgb&w=600",
-      image2:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      id: 3,
-      title: "Image 3",
-      image1:
-        "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=600",
-      image2:
-        "https://images.pexels.com/photos/1381556/pexels-photo-1381556.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    // {
-    //   id: 4,
-    //   title: "Image 4",
-    //   image1: "https://images.pexels.com/photos/1034859/pexels-photo-1034859.jpeg?auto=compress&cs=tinysrgb&w=600",
-    //   image2: "https://images.pexels.com/photos/972804/pexels-photo-972804.jpeg?auto=compress&cs=tinysrgb&w=600",
-    // },
-    // {
-    //   id: 5,
-    //   title: "Image 5",
-    //   image1: "https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=600",
-    //   image2: "https://images.pexels.com/photos/413959/pexels-photo-413959.jpeg?auto=compress&cs=tinysrgb&w=600",
-    // }
-  ];
+ 
 
   return (
     <>
