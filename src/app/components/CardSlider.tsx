cb35de88b7a3365d4db8d1e680d6958c46211d0d
vinod-r07 <vkumar.vinod2021@gmail.com>
2024-09-22 21:25:52 +0530

    6th

--- src/app/components/CardSlider.tsx
+++ src/app/components/CardSlider.tsx
@@ -1,52 +1,52 @@
-// import Link from "next/link";
-import React from "react";
-// import { data } from "../data";
-import Slider from "react-slick";
+// // import Link from "next/link";
+// import React from "react";
+// // import { data } from "../data";
+// import Slider from "react-slick";
 
 
 
-function CardSlider() {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 750,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 425,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-  return (
-    <div>
-      <Slider {...settings}>
-        <h3>1</h3>
-        <h3>2</h3>
-        <h3>3</h3>
-        <h4>4</h4>
+// function CardSlider() {
+//   const settings = {
+//     dots: false,
+//     infinite: false,
+//     speed: 500,
+//     slidesToShow: 4,
+//     slidesToScroll: 2,
+//     responsive: [
+//       {
+//         breakpoint: 750,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 600,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 425,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//   };
+//   return (
+//     <div>
+//       <Slider {...settings}>
+//         <h3>1</h3>
+//         <h3>2</h3>
+//         <h3>3</h3>
+//         <h4>4</h4>
         
-      </Slider>
-      </div>
-  );
-}
+//       </Slider>
+//       </div>
+//   );
+// }
 
-export default CardSlider;
+// export default CardSlider;
--- src/app/components/SliderBanner.tsx
+++ src/app/components/SliderBanner.tsx
@@ -1,57 +1,57 @@
-import React from "react";
-import Slider from "@ant-design/react-slick";
-import Image from "next/image";
+// import React from "react";
+// import Slider from "@ant-design/react-slick";
+// import Image from "next/image";
 
 
 
-function SliderBanner() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-  };
-  return (
-    <Slider {...settings}>
-      <div>
-        <Image
-        width={100} height={100}
-          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/61F97E9098730C6E359AC4FDD76D9BC92B2433346909A818802A9B8EDB02AA97/compose?width=1440&aspectRatio=3.91&format=jpeg&label=staroriginal_391_scrim"
-          alt="badag"
-        />
-      </div>
-      <div>
-        <Image
-        width={100} height={100}
-          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/757BD50DDE15AEA20A1DBA02E4A7BEE497E14BB8B1E27217F6FEA384CD28256E/compose?width=1440&aspectRatio=3.91&format=jpeg&label=disneyplusoriginal_391_scrim"
-          alt="avengers"
-        />
-      </div>
-      <div>
-        <Image
-        width={100} height={100}
-          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C166EEA681E3AA1DF16788BDF7ED724D5D40E9D088CB7F52A1BFDC38BC63FE37/compose?width=1440&aspectRatio=3.91&format=jpeg&label=disneyplusoriginal_391_scrim"
-          alt="badging"
-        />
-      </div>
-      <div>
-        <Image
-        width={100} height={100}
-          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7487B12724430E5DF3FBDBC9D1E1F9334674F469BA03CEFCFF700138773493C0/scale?width=1440&aspectRatio=3.91&format=jpeg"
-          alt="scale"
-        />
-      </div>
-      <div>
-        <Image
-        width={100} height={100}
-          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7450DF0759679B9050CE04F4037CD56C4B1128DE2071FD7FE5EF58C90A8B0A65/scale?width=1440&aspectRatio=3.91&format=jpeg"
-          alt="scales"
-        />
-      </div>
-    </Slider>
-  );
-}
+// function SliderBanner() {
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//   };
+//   return (
+//     <Slider {...settings}>
+//       <div>
+//         <Image
+//         width={100} height={100}
+//           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/61F97E9098730C6E359AC4FDD76D9BC92B2433346909A818802A9B8EDB02AA97/compose?width=1440&aspectRatio=3.91&format=jpeg&label=staroriginal_391_scrim"
+//           alt="badag"
+//         />
+//       </div>
+//       <div>
+//         <Image
+//         width={100} height={100}
+//           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/757BD50DDE15AEA20A1DBA02E4A7BEE497E14BB8B1E27217F6FEA384CD28256E/compose?width=1440&aspectRatio=3.91&format=jpeg&label=disneyplusoriginal_391_scrim"
+//           alt="avengers"
+//         />
+//       </div>
+//       <div>
+//         <Image
+//         width={100} height={100}
+//           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C166EEA681E3AA1DF16788BDF7ED724D5D40E9D088CB7F52A1BFDC38BC63FE37/compose?width=1440&aspectRatio=3.91&format=jpeg&label=disneyplusoriginal_391_scrim"
+//           alt="badging"
+//         />
+//       </div>
+//       <div>
+//         <Image
+//         width={100} height={100}
+//           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7487B12724430E5DF3FBDBC9D1E1F9334674F469BA03CEFCFF700138773493C0/scale?width=1440&aspectRatio=3.91&format=jpeg"
+//           alt="scale"
+//         />
+//       </div>
+//       <div>
+//         <Image
+//         width={100} height={100}
+//           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7450DF0759679B9050CE04F4037CD56C4B1128DE2071FD7FE5EF58C90A8B0A65/scale?width=1440&aspectRatio=3.91&format=jpeg"
+//           alt="scales"
+//         />
+//       </div>
+//     </Slider>
+//   );
+// }
 
-export default SliderBanner;
+// export default SliderBanner;
