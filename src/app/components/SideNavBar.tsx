e07a27b3d323828fc13d2eb642c01e553ece6d30
Atam Swaroop <112094449+agamswaroop619@users.noreply.github.com>
2024-12-11 11:38:04 +0530

    fix: styling

--- src/app/components/SideNavBar.tsx
+++ src/app/components/SideNavBar.tsx
@@ -12,55 +12,69 @@ const SideNavBar: React.FC<PropsType> = ({ setSideNav }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="z-10 lg:w-0 md:w-0 xl:w-0 sm:w-[60vw] xs:w-[70vw]  w-[100vw] py-10 px-5 bg-white h-[100vh] flex flex-col text-black">
+    <div className="z-10 lg:w-0 md:w-0 xl:w-0 sm:w-[60vw] xs:w-[70vw]  w-[100vw] py-10 px-5 bg-black h-[100vh] flex flex-col text-white">
+      <div className="full mb-6 relative">
+        <RiCloseLargeFill
+          className="absolute right-4"
+          onClick={() => setSideNav(false)}
+        />
+      </div>
 
+      <Link href="/">Home</Link>
 
-        <div className="full mb-6 relative">
-          <RiCloseLargeFill className="absolute right-4" onClick={() => setSideNav(false)} />
+      <div>
+        <div className="flex gap-4 mt-2" onClick={() => setOpen(!open)}>
+          <span>Collections</span>
+          <GoChevronDown
+            className={`  ${
+              open === true
+                ? " transition duration-500 ease rotate-180"
+                : "transition duration-500 ease rotate-0"
+            }`}
+          />
         </div>
 
-        <Link href="/">Home</Link>
-
-        <div>
-          <div className="flex gap-4 mt-2" onClick={() => setOpen(!open)}>
-            <span>Collections</span>
-            <GoChevronDown className={`  ${
-                open === true ? " transition duration-500 ease rotate-180" : "transition duration-500 ease rotate-0"
-              }`} />
-          </div>
-
-          {open ? (
-            <ul
+        {open ? (
+          <ul
             className={`mx-3 px-2 border-l-2 border-slate-400 transition-all duration-500 ease ${
-              open ? 'opacity-100 max-h-[500px]' : 'opacity-0 max-h-0 overflow-hidden'
+              open
+                ? "opacity-100 max-h-[500px]"
+                : "opacity-0 max-h-0 overflow-hidden"
             }`}
           >
-             <li>
-                <Link className="my-2" href="/shop">Oversized Tshirts</Link>
-              </li>
+            <li>
+              <Link className="my-2" href="/shop">
+                Oversized Tshirts
+              </Link>
+            </li>
 
-              <li>
-                <Link className="my-2" href="/shop/regular">Regular Tshirts</Link>
-              </li>
+            <li>
+              <Link className="my-2" href="/shop/regular">
+                Regular Tshirts
+              </Link>
+            </li>
 
-              <li>
-                <Link className="my-2" href="/shop/polo">Polo</Link>
-              </li>
+            <li>
+              <Link className="my-2" href="/shop/polo">
+                Polo
+              </Link>
+            </li>
 
-              <li>
-                <Link className="my-2" href="/shop/psychedelics">Psychedelics Tshirts</Link>
-              </li>
-            </ul>
-          ) : null}
-        </div>
+            <li>
+              <Link className="my-2" href="/shop/psychedelics">
+                Psychedelics Tshirts
+              </Link>
+            </li>
+          </ul>
+        ) : null}
+      </div>
 
-        <div className="flex flex-col my-2 gap-y-2">
-          <Link href="/">Abouts us</Link>
-          <Link href="/contacts">Contact us</Link>
-          <Link href="/track-order">Track order</Link>
-          <Link href="/login">Login/Sign up</Link>
-        </div>
-
+      <div className="flex flex-col my-2 gap-y-2">
+        <Link href="/">Abouts us</Link>
+        <Link href="/contacts">Contact us</Link>
+        <Link href="/track-order">Track order</Link>
+        <Link href="/login">Login/Sign up</Link>
+      </div>
     </div>
   );
 };
