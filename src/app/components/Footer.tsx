20882b46b19e8a801daab6b9eaa2d5666a1c041c
Atam Swaroop <112094449+agamswaroop619@users.noreply.github.com>
2025-01-12 20:22:30 +0530

    fix: Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,48 +1,76 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-700 flex flex-col w-[100vw] mb-0  gap-y-4" >
-      
-        <div className="flex md:flex-row lg:flex-row xl:flex-row xs:flex-col sm:flex-col p-6 w-full justify-around">
-          <div className="lg:w-4/12 " >
-            <h2 className="text-green-600  my-4 text-2xl ">Join our club </h2>
-            <p className="mb-4">By Submittng your email, you agree to receive advertising emails from Sway. </p>
+    <footer className="bg-gray-700 flex flex-col w-[100vw] mb-0  gap-y-4">
+      <div className="flex md:flex-row lg:flex-row xl:flex-row xs:flex-col sm:flex-col p-6 w-full justify-around">
+        <div className="lg:w-4/12 ">
+          <h2 className="text-green-600  my-4 text-2xl ">Join our club </h2>
+          <p className="mb-4">
+            By Submittng your email, you agree to receive advertising emails
+            from Sway.{" "}
+          </p>
 
-            <p className="my-14 ">© 2024 Sway. All Rights Reserved. </p>
-          </div>
+          <p className="my-14 ">© 2024 Sway. All Rights Reserved. </p>
+        </div>
 
-          <div className="lg:w-3/12">
-            <h3 className="text-green-600 my-4 text-xl ">About Sway</h3>
-            <ul className="flex flex-col gap-y-4">
-              <li> <a href="/shop/streetwears">Streetwear Collection</a> </li>
-              <li> <a href="#">Blogs</a> </li>
-              <li> <a href="/carts">Cart</a> </li>
-              <li> <a href="/carts">Checkout</a> </li>
-              <li> <a href="/wishlist">Wishlist</a> </li>
-              <li> <a href="/privacy-policy">Privacy Policy</a> </li>
-            </ul>
-          </div>
+        <div className="lg:w-3/12">
+          <h3 className="text-green-600 my-4 text-xl ">About Sway</h3>
+          <ul className="flex flex-col gap-y-4">
+            {/* <li> <a href="/shop/streetwears">Streetwear Collection</a> </li> */}
+            {/* <li> <a href="#">Blogs</a> </li> */}
+            <li>
+              {" "}
+              <a href="/carts">Cart</a>{" "}
+            </li>
+            {/* <li> <a href="/carts">Checkout</a> </li> */}
+            <li>
+              {" "}
+              <a href="/wishlist">Wishlist</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/privacy-policy">Privacy Policy</a>{" "}
+            </li>
+          </ul>
+        </div>
 
-          <div className="lg:w-3/12">
+        <div className="lg:w-3/12">
           <h3 className="text-green-600 my-4 text-xl ">Help & Support</h3>
-            <ul className="flex flex-col gap-y-4">
-              <li> <a href="/about">About us</a> </li>
-              <li> <a href="/contacts">Contact us</a> </li>
-              <li> <a href="/profile">My account</a> </li>
-              <li> <a href="/terms-conditions">Terms & Conditions</a> </li>
-              <li> <a href="/refund_returns/">Refund and Returns Policy</a> </li>
-              <li> <a href="/shipping-policy">Shipping Policy</a> </li>
-            </ul>
-          </div>
+          <ul className="flex flex-col gap-y-4">
+            <li>
+              {" "}
+              <a href="/about">About us</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/contacts">Contact us</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/profile">My account</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/terms-conditions">Terms & Conditions</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/refund_returns/">Refund and Returns Policy</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/shipping-policy">Shipping Policy</a>{" "}
+            </li>
+          </ul>
         </div>
+      </div>
 
-        <div className="mx-4 p-2 border-t-2">
-          <p className="text-center tracking-wide">Copyright © 2024 Sway </p>
-        </div>
+      <div className="mx-4 p-2 border-t-2">
+        <p className="text-center tracking-wide">Copyright © 2024 Sway </p>
+      </div>
+    </footer>
+  );
+};
 
-        </footer>
-  )
-}
-
-export default Footer
+export default Footer;
